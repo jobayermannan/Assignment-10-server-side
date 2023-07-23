ffcc1523f227ea784bd779b7d9b7be71ce8b7778
Jobayer Mannan <jobayermannan777@gmail.com>
2023-07-23 21:40:28 +0600

    server set up done and server config done

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
 const express = require('express')
 const app = express()
-const port = 3000
+var cors = require('cors')
+const port =process.env.PORT || 5000;
+
+
+
+const lists = require('./data/lists.json')
+const chefs = require('./data/chef.json')
+
+
+
+
+app.use(cors());
+
+
+app.get('/lists', (req, res) =>{
+  res.send(lists)
+})
+app.get('/chefs', (req, res) =>{
+  res.send(chefs)
+})
+app.get('/chefs/:id', (req, res) =>{
+  const id = req.params.id;
+   const selectedChef =chefs.find(c => c._id === id);
+   res.send(selectedChef);
+})
+app.get('/lists/:id', (req, res) =>{
+  const id =parseInt(req.params.id) ;
+ 
+   if (id === 0){
+    res.send (chefs)
+   }else {
+    const selectedList =chefs.filter(c => parseInt(c.listId) === (id));
+    res.send(selectedList);
+   }
+  
+
+})
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
-  res.send('Chef server is running at http://localhost')
+  res.send('Chef server is running ')
 })
 
 app.listen(port, () => {
